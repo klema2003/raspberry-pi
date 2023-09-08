77737d78bf0fd93a5e9d0ce9227f17e14b3da1b7
pekle1 <93258143+pekle1@users.noreply.github.com>
2023-09-08 16:21:40 +0200

    dht11_test

--- dht11/dht11_test.py
+++ dht11/dht11_test.py
@@ -1,76 +1,41 @@
-#include <wiringPi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#define MAXTIMINGS	85
-#define DHTPIN		7
-int dht11_dat[5] = { 0, 0, 0, 0, 0 };
- 
-void read_dht11_dat()
-{
-	uint8_t laststate	= HIGH;
-	uint8_t counter		= 0;
-	uint8_t j		= 0, i;
-	float	f; 
- 
-	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
- 
-	pinMode( DHTPIN, OUTPUT );
-	digitalWrite( DHTPIN, LOW );
-	delay( 18 );
-	digitalWrite( DHTPIN, HIGH );
-	delayMicroseconds( 40 );
-	pinMode( DHTPIN, INPUT );
- 
-	for ( i = 0; i < MAXTIMINGS; i++ )
-	{
-		counter = 0;
-		while ( digitalRead( DHTPIN ) == laststate )
-		{
-			counter++;
-			delayMicroseconds( 1 );
-			if ( counter == 255 )
-			{
-				break;
-			}
-		}
-		laststate = digitalRead( DHTPIN );
- 
-		if ( counter == 255 )
-			break;
- 
-		if ( (i >= 4) && (i % 2 == 0) )
-		{
-			dht11_dat[j / 8] <<= 1;
-			if ( counter > 16 )
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	}
- 
-	if ( (j >= 40) &&
-	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-	{
-		f = dht11_dat[2] * 9. / 5. + 32;
-		printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-			dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
-	}else  {
-		printf( "Data not good, skip\n" );
-	}
-}
- 
-int main( void )
-{
-	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
- 
-	if ( wiringPiSetup() == -1 )
-		exit( 1 );
- 
-	while ( 1 )
-	{
-		read_dht11_dat();
-		delay( 1000 ); 
-	}
- 
-	return(0);
-}
+import RPi.GPIO as GPIO
+import dht11
+import time
+
+DHT_PIN = 5
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+# GPIO.cleanup()
+
+#GPIO.setup(DHT_PIN ,GPIO.IN,pull_up_down= GPIO.PUD_DOWN)
+
+instance = dht11.DHT11(pin= DHT_PIN)
+#instance = dht11.DHT11(pin=6)
+time.sleep(2)
+
+filename = "./py/dht11.temp"
+temp = 0
+humi = 0
+num = 0
+
+with open(filename, 'w') as f:
+
+    for i in range(3):
+        result = instance.read()
+        if result.is_valid():
+            print("%d :" % i + "Temp: %d C" % result.temperature + ' '+"Humid: %d %%" % result.humidity)
+            temp += result.temperature
+            humi += result.humidity
+            num += 1
+
+        time.sleep(2)
+    if num > 0:    
+        f.write("Temp:" + str(temp / num) + " C,Hum:" + str(humi / num) + " %.\n")
+    else :
+        f.write("Read nothing, wait and read again.\n")
+
+    f.flush()
+
+    f.close()
